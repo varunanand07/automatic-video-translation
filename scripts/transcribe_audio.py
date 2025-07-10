a89df3c15246652c793f56d862ff826e2562815e
varunanand07 <anandvarun2003@gmail.com>
2025-07-10 19:36:51 +0100

    Added saving transcripts, missing folders crashed the program

--- scripts/transcribe_audio.py
+++ scripts/transcribe_audio.py
@@ -1,5 +1,6 @@
 import whisper
 import yt_dlp
+import os
 
 video_urls = [
     "https://www.youtube.com/watch?v=-moW9jvvMr4&pp=ygUJdGVkIHRhbGtz",
@@ -9,8 +10,12 @@ video_urls = [
 
 model = whisper.load_model("base")
 
+audio_dir = "data/audio"
+transcript_dir = "data/asr_transcripts"
+
 ydl_opts = {
     'format': 'bestaudio',
+    'outtmpl': f'{audio_dir}/%(id)s.%(ext)s',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
@@ -21,6 +26,8 @@ with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     for url in video_urls:
         info = ydl.extract_info(url, download=True)
         video_id = info['id']
-        audio_file = f"{video_id}.mp3" 
-        result = model.transcribe(audio_file)
-        print(result["text"]) 
+        audio_path = f"{audio_dir}/{video_id}.mp3"
+        result = model.transcribe(audio_path)
+
+        with open(f"{transcript_dir}/{video_id}.txt", "w", encoding="utf-8") as f:
+            f.write(result["text"])
